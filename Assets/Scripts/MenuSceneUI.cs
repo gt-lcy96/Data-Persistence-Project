cee7f9beab007ff57100fdf73b6479efa8a955e0
gt-lcy96 <lohcy96@gmail.com>
2023-01-22 07:02:15 -0800

    Add start button function

--- Assets/Scripts/MenuSceneUI.cs
+++ Assets/Scripts/MenuSceneUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuSceneUI : MonoBehaviour
 {
@@ -21,5 +22,10 @@ public class MenuSceneUI : MonoBehaviour
     {
         nameInput = s;
         Debug.Log(nameInput);
+    }
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
     }
 }
